3f1b0ffd65c3ee159452821036f9a6f4cd025384
pauk-nsk <sintez-kr85@mail.ru>
2025-10-02 22:07:33 +0700

    site17

--- main/wsgi.py
+++ main/wsgi.py
@@ -12,15 +12,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
-if 'RENDER' in os.environ:
-    try:
-        from startup import run_migrations
-
-        run_migrations()
-    except Exception as e:
-        print(f"Startup migration failed: {e}")
-
-application = get_wsgi_application()
-
 application = get_wsgi_application()
